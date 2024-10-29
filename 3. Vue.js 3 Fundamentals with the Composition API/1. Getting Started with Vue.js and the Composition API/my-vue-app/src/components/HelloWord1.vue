4e76e5960ed87dd3febe6f89bf97887a3d473fb7
Audyari Wiyono <audy123ari@gmail.com>
2024-10-29 14:46:11 +0700

    2. Vue Template Syntax and Expressions

--- 3. Vue.js 3 Fundamentals with the Composition API/1. Getting Started with Vue.js and the Composition API/my-vue-app/src/components/HelloWord1.vue
+++ 3. Vue.js 3 Fundamentals with the Composition API/1. Getting Started with Vue.js and the Composition API/my-vue-app/src/components/HelloWord1.vue
@@ -5,5 +5,7 @@ const header = ref('Shopping List App')
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
+  <h1>{{ header.toUpperCase() }}</h1>
+  <h1>{{ header.toLocaleUpperCase() }}</h1>
+  <input type="text" v-model="header">
 </template>
