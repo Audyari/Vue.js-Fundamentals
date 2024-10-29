5eff3e576965affd7ade24cd14fb2e554f153d49
Audyari Wiyono <audy123ari@gmail.com>
2024-10-29 14:39:11 +0700

    1. Getting Started with Vue.js and the Composition API

---
+++ 3. Vue.js 3 Fundamentals with the Composition API/1. Getting Started with Vue.js and the Composition API/my-vue-app/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <HelloWord1 />
+  </div>
+</template>
+
+<script setup>
+import HelloWord1 from './components/HelloWord1.vue'
+import './assets/styles.css'; 
+</script>
